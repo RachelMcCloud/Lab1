d49b0bf29b390312a85206aeb8aed6f324c6122a
Rachel McCloud <[Drachelmccloud22@gmail.com>
2020-07-14 09:43:04 -0400

    Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string goAgain;
+            do
+            {
+                Console.Write("Please enter width (in feet): ");
+                string width = Console.ReadLine();
+                Console.Write("Please enter length (in feet): ");
+                string length = Console.ReadLine();
+                double perimeter = (2 * double.Parse(width)) + (2 * double.Parse(length));
+                double area = double.Parse(width) * double.Parse(length);
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+                Console.WriteLine("\no");
+                Console.WriteLine("Would you like to continue? yes or no");
+                goAgain = Console.ReadLine().ToLower();
+                Console.WriteLine("\no");
+            } while (goAgain == "yes");
+            Console.WriteLine("Goodbye!");
+            Console.ReadKey();
+        }
+    }
+}
+        
+   
